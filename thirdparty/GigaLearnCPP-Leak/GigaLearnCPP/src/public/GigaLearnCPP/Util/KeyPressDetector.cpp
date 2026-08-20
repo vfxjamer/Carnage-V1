29c7eca966ef4d63210ac8df74cd2c04d00f3f15
vfxjamer <vfxjamer@users.noreply.github.com>
2026-08-20 13:07:39 +0530

    Disable interactive keypress reading on non-MSVC builds (kills tcsetattr spam on Colab non-TTY stdin)

--- thirdparty/GigaLearnCPP-Leak/GigaLearnCPP/src/public/GigaLearnCPP/Util/KeyPressDetector.cpp
+++ thirdparty/GigaLearnCPP-Leak/GigaLearnCPP/src/public/GigaLearnCPP/Util/KeyPressDetector.cpp
@@ -8,28 +8,11 @@ char GGL::KeyPressDetector::GetPressedChar() {
 }
 
 #else
-#include <unistd.h>
-#include <termios.h>
-
+// Non-MSVC (Linux/Colab): no interactive key reads. Training is controlled via
+// signals (or the notebook's STOP cell), not keyboard input. Calling tcsetattr
+// on a non-TTY stdin (as done previously) spams "Inappropriate ioctl" errors.
 char GGL::KeyPressDetector::GetPressedChar() {
-	// https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
-	char buf = 0;
-	struct termios old = { 0 };
-	if (tcgetattr(0, &old) < 0)
-		perror("tcsetattr()");
-	old.c_lflag &= ~ICANON;
-	old.c_lflag &= ~ECHO;
-	old.c_cc[VMIN] = 1;
-	old.c_cc[VTIME] = 0;
-	if (tcsetattr(0, TCSANOW, &old) < 0)
-		perror("tcsetattr ICANON");
-	if (read(0, &buf, 1) < 0)
-		perror("read()");
-	old.c_lflag |= ICANON;
-	old.c_lflag |= ECHO;
-	if (tcsetattr(0, TCSADRAIN, &old) < 0)
-		perror("tcsetattr ~ICANON");
-	return (buf);
+	return 0;
 }
 
 #endif
